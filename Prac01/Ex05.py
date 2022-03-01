95f3eff4d3fd79a6ff9c488986d26fb62efb183b
rec.fffixing <imdex1009@gmail.com>
2022-03-02 02:10:08 +0900

    updated

---
+++ Prac01/Ex05.py
@@ -0,0 +1,11 @@
+data = dict()
+data['사과'] = "Apple"
+data['바나나'] = "Banana"
+data['코코넛'] = 'Coconut'
+
+# 키 데이터만 담은 리스트
+key_list = data.keys()
+
+value_list = data.values()
+print(key_list)
+print(value_list)
